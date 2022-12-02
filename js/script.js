179444e207ba79f2b139c66cf159677ccfa8d961
parsa-tahavori <113055869+parsa-tahavori@users.noreply.github.com>
2022-12-02 14:18:05 -0500

    ,,,...,

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-PWA-Test-use/sw.js", {
+    scope: "/ICS2O-PWA-Test-use/",
   })
 }
 
